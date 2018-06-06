52eebfa3fae634e3d0d3b2117232249676708ccb
Hans <shaomenghao@hotmail.com>
2018-06-06 22:24:11 +0800

    OcelotWithConsul

---
+++ OcelotWithConsul/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OcelotWithConsul
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, config) =>
+                {
+                    config.SetBasePath(host.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("Ocelot.json");
+                })
+                .UseStartup<Startup>();
+    }
+}
